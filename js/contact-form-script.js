4926249b50bad0160f115f09d5666e6c0826cf14
Sanjay Rawat <13738294+RawSanj@users.noreply.github.com>
2021-09-10 17:32:55 +0530

    Update contact form to send email

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -11,42 +11,59 @@ $("#contactForm").validator().on("submit", function (event) {
 });
 
 
-function submitForm(){
+function submitForm() {
     // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var msg_subject = $("#msg_subject").val();
-    var message = $("#message").val();
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const guest = $("#guest").find(":selected").text();
+    const rsvp = $("#rsvp").find(":selected").text();
+    const message = $("#message").val();
 
+    const subject = name + " says " + rsvp;
+    const email_message = email + "\nTotal Guests: " + guest + "\n" + message;
+
+    const jsonData = {
+        "toEmails": [
+            "example@example.com"
+        ],
+        "subject": subject,
+        "message": email_message
+    };
 
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
+        url: "https://dl6gsolzfd.execute-api.ap-south-1.amazonaws.com/Prod/send",
+        data: JSON.stringify(jsonData),
+        headers: {
+            'X-Api-Key': 'rjp5GUfhhSOgEQdZ1DkD4SzzTEPEYwz40iHxMETj'
+        },
+        contentType: "application/json; charset=utf-8",
+        success: function (text) {
+            console.log(`Response: ${text}`);
+            if (text === "Planner Notified") {
                 formSuccess();
             } else {
                 formError();
-                submitMSG(false,text);
+                submitMSG(false, text);
             }
         }
     });
 }
 
-function formSuccess(){
+function formSuccess() {
     $("#contactForm")[0].reset();
     submitMSG(true, "Message Submitted!")
+    $('#submit').hide();
 }
 
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+function formError() {
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $(this).removeClass();
     });
 }
 
-function submitMSG(valid, msg){
-    if(valid){
+function submitMSG(valid, msg) {
+    if (valid) {
         var msgClasses = "h3 text-center tada animated text-success";
     } else {
         var msgClasses = "h3 text-center text-danger";
