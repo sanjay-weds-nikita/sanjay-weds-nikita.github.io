a07a5d0d70dd625d76783f57344454204be5ce81
Sanjay Rawat <13738294+RawSanj@users.noreply.github.com>
2021-09-10 18:51:33 +0530

    Add Social links and New Logo

--- js/custom.js
+++ js/custom.js
@@ -113,18 +113,18 @@
     Countdown Clock
     ................................................. */
 	function makeTimer() {
-		var endTime = new Date("15 November 2021 10:00:00 GMT+05:30");
+		let endTime = new Date("15 November 2021 10:00:00 GMT+05:30");
 		endTime = (Date.parse(endTime) / 1000);
 
-		var now = new Date();
+		let now = new Date();
 		now = (Date.parse(now) / 1000);
 
-		var timeLeft = endTime - now;
+		const timeLeft = endTime - now;
 
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		const days = Math.floor(timeLeft / 86400);
+		let hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		let minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+		let seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
 		if (hours < "10") { hours = "0" + hours; }
 		if (minutes < "10") { minutes = "0" + minutes; }
